615455982bb7f7d45ace4bcc26ea6fee6a1f9c90
ReginYuan <49477488+ReginYuan@users.noreply.github.com>
2024-03-14 15:21:27 +0800

    feat: 模板分支

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+<script  lang="ts">
+import { defineComponent, ref, reactive } from "vue";
+interface Person {
+  name: string,
+  age: number
+}
+export default defineComponent({
+  name: "App",
+  setup() {
+    const count = ref<number | string>(0)
+    const incrment = () => {
+      count.value++
+    }
+    const user: Person = reactive({
+      name: 'ReginYuan',
+      age: 25
+    })
+    return {
+      count,
+      incrment,
+      user
+    }
+  }
+})
 </script>
 
 <template>
@@ -10,6 +32,10 @@ import HelloWorld from "./components/HelloWorld.vue";
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <h1>{{ user.name }}</h1>
+    <h1>{{ user.age }}</h1>
+    <h1>{{ count }}</h1>
+    <button @click="incrment">新增</button>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
@@ -21,9 +47,11 @@ import HelloWorld from "./components/HelloWorld.vue";
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
